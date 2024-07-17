28ea82e0d9f25d62d196016a95b20460d15ef39d
Farendivaz <dandyfariz9@gmail.com>
2024-07-17 09:42:55 +0700

    Fix(deployment)

---
+++ _next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"KUlZqZaMa8Fg2sqxfOp+Vi/o75ldTrX5qpD8xAlqCjU=\"\n}"
--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: "build",
+  distDir: "_next",
 };
 
 export default nextConfig;
