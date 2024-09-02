cc482d79e0b87fcb2b2f87036243cc81b3244681
farendivaz <dandyfariz9@gmail.com>
2024-09-02 09:31:06 +0700

    Fix: Profile card

--- app/(about)/tentang/page.tsx
+++ app/(about)/tentang/page.tsx
@@ -92,7 +92,7 @@ export default async function Tentang() {
         Board Of Commissioner
       </h1>
       <section className="w-full bg-white text-black text-justify flex justify-center items-center">
-        <section className="w-4/5 mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 justify-center items-center">
+        <section className="w-1/2 mx-auto grid grid-cols-1 lg:grid-cols-2 justify-center items-center">
           <ProfileCard
             name="Donald Akbar"
             grid={3}
