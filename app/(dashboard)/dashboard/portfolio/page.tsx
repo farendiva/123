1da811e5e9c97243d27116b023d32cc00c16fa56
farendivaz <dandyfariz9@gmail.com>
2024-10-08 23:34:41 +0700

    Fix: tenorr, profile card, & wording sukuk

--- app/(dashboard)/dashboard/portfolio/page.tsx
+++ app/(dashboard)/dashboard/portfolio/page.tsx
@@ -1,6 +1,6 @@
-import PortfolioList from "@/app/components/dashboard/PortfolioList";
-import { getPortfolios } from "@/lib/listing";
-import { cookies } from "next/headers";
+// import PortfolioList from "@/app/components/dashboard/PortfolioList";
+// import { getPortfolios } from "@/lib/listing";
+// import { cookies } from "next/headers";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -8,9 +8,9 @@ export const metadata: Metadata = {
   description: "Portfolio Pemodal FULUSME",
 };
 export default async function PortfolioPage() {
-  const cookieStore = cookies();
-  const token = cookieStore.get("authToken")?.value;
-  const { data } = await getPortfolios(token as string);
+  // const cookieStore = cookies();
+  // const token = cookieStore.get("authToken")?.value;
+  // const { data } = await getPortfolios(token as string);
   return (
     <main className="w-full mx-auto rounded-xl">
       {/* <PortfolioList data={data.data} /> */}
